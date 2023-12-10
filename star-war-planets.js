4ec9c1e464701bddf07966e1f48d4bcee8afacfa
Sadikibenda <Sadiki.benda1@gmail.com>
2023-12-09 21:45:12 -0500

    update

--- star-war-planets.js
+++ star-war-planets.js
@@ -1,13 +1,16 @@
 let starWarsPlanetsList = document.querySelector("ul")
 
 fetch("https://swapi.dev/api/planets")
+
  .then(response => response.json())
  .then(function(json){
-    let planets = json.results;
-
-    for(p of planets) {
+    //console.log(json)
+    let search = json.results;
+      
+    for(p of search) {
+        console.log(p)
         let listItem = document.createElement('li');
-        listItem.innerHTML = p.name;
+        listItem.innerHTML = p.name //+ "<br />" + p.name;
         starWarsPlanetsList.appendChild(listItem)
     }
  })
